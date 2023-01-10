e82e792b47b82f99fad13e6373c6f4f969bdd1f6
Barcia <fran.barcia@dxc.com>
2023-01-10 20:43:39 +0100

    Segundo commit

--- myproject/myapp/urls.py
+++ myproject/myapp/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path, include
 from rest_framework import routers
 from .views import CharacterViewSet
+from .views import my_view
+
 router = routers.DefaultRouter()
-router.register(r'mymodel', CharacterViewSet)
+router.register(r'characters', CharacterViewSet)
 urlpatterns = [
     path('', include(router.urls)),
+    path('search-character-appearance', my_view)
 ]
--- myproject/myapp/views.py
+++ myproject/myapp/views.py
@@ -2,6 +2,16 @@ from django.shortcuts import render
 from rest_framework import viewsets
 from .models import Character
 from .serializers import CharacterSerializer
+from django.http import HttpResponse
+
 class CharacterViewSet(viewsets.ModelViewSet):
     queryset = Character.objects.all()
     serializer_class = CharacterSerializer
+
+def my_view(request):
+    name = request.GET.get('name')
+    
+    if name:
+        return HttpResponse(f'Hello, {name}!')
+    else:
+        return HttpResponse('Hello, anonymous!')
